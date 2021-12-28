a20034e13e8c99d503abed92975816e19dcc1389
fasc8 <16879988+fasc8@users.noreply.github.com>
2021-12-28 20:47:52 +0100

    remove .mounts.sh to make it possible to run custom script on system also rename .mounts.sh to .startup.sh

--- .mounts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/vmhgfs-fuse .host:/Projects /home/manjaro/Projects -o subtype=vmhgfs-fuse,allow_other
-/usr/bin/vmhgfs-fuse .host:/Hochschule /home/manjaro/Hochschule/ -o subtype=vmhgfs-fuse,allow_other
