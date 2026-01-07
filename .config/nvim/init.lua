bf54c62978bb093700df3c3db1c5cf8b822fe08c
fasc8 <16879988+fasc8@users.noreply.github.com>
2026-01-07 14:13:21 +0100

    switch to telescope for finding and lsp search

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -385,8 +385,30 @@ require("lazy").setup({
                 })
             end)
         end
-    }, -- LSP
-    {
+    }, {
+        'nvim-telescope/telescope.nvim',
+        tag = 'v0.2.1',
+        dependencies = {
+            'nvim-lua/plenary.nvim', -- optional but recommended
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files,
+                           {desc = 'Telescope find files'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+                           {desc = 'Telescope live grep'})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers,
+                           {desc = 'Telescope buffers'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+                           {desc = 'Telescope help tags'})
+
+            vim.keymap.set('n', 'gd', builtin.lsp_definitions)
+            vim.keymap.set('n', 'gi', builtin.lsp_implementations)
+            vim.keymap.set('n', 'gr', builtin.lsp_references)
+            vim.keymap.set('n', 'gf', builtin.treesitter)
+        end
+    }, { -- LSP
         'neovim/nvim-lspconfig',
         config = function()
             -- Setup language servers.
@@ -517,9 +539,11 @@ require("lazy").setup({
                     -- See `:help vim.lsp.*` for documentation on any of the below functions
                     local opts = {buffer = ev.buf}
                     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    -- These are handled with telescope
+                    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help,
                                    opts)
                     vim.keymap.set('n', '<leader>wa',
@@ -533,7 +557,6 @@ require("lazy").setup({
                     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
                     vim.keymap.set({'n', 'v'}, '<leader>a',
                                    vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                     vim.keymap.set('n', '<leader>f', function()
                         vim.lsp.buf.format {async = true}
                     end, opts)
