47bcf881866f0ea522d735b1de2eee69d8dde831
fasc8 <16879988+fasc8@users.noreply.github.com>
2026-05-05 08:44:18 +0200

    adds gitsigns to show git status in repositories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1171,7 +1171,100 @@ require("lazy").setup({
         dependencies = {'nvim-treesitter'},
         config = function() require("nvim-ts-autotag").setup() end
     }, {"mg979/vim-visual-multi", branch = "master", event = "VeryLazy"},
-    {"bronson/vim-visual-star-search", event = "VeryLazy"}
+    {"bronson/vim-visual-star-search", event = "VeryLazy"}, {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = {text = '┃'},
+                change = {text = '┃'},
+                delete = {text = '_'},
+                topdelete = {text = '‾'},
+                changedelete = {text = '~'},
+                untracked = {text = '┆'}
+            },
+            signs_staged = {
+                add = {text = '┃'},
+                change = {text = '┃'},
+                delete = {text = '_'},
+                topdelete = {text = '‾'},
+                changedelete = {text = '~'},
+                untracked = {text = '┆'}
+            },
+            signs_staged_enable = true,
+            signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+            numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+            linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+            watch_gitdir = {follow_files = true},
+            auto_attach = true,
+            attach_to_untracked = false,
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true
+            },
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+            blame_formatter = nil, -- Use default
+            sign_priority = 6,
+            update_debounce = 100,
+            status_formatter = nil, -- Use default
+            max_file_length = 40000, -- Disable if file is longer than this (in lines)
+            preview_config = {
+                -- Options passed to nvim_open_win
+                style = 'minimal',
+                relative = 'cursor',
+                row = 0,
+                col = 1
+            }
+        },
+        config = function()
+            local gitsigns = require('gitsigns')
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']g', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({']c', bang = true})
+                else
+                    gitsigns.nav_hunk('next')
+                end
+            end)
+
+            map('n', '[g', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({'[c', bang = true})
+                else
+                    gitsigns.nav_hunk('prev')
+                end
+            end)
+
+            map('n', '<leader>hp', gitsigns.preview_hunk)
+            map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+            map('n', '<leader>hb',
+                function() gitsigns.blame_line({full = true}) end)
+
+            map('n', '<leader>hd', gitsigns.diffthis)
+
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+
+            -- Toggles
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+            map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+            -- Text object
+            map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+        end
+    }
 
 })
 
