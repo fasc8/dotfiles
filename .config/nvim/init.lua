a1deda6c731f21b9144ef9f5c3735b2690b0d4a1
fasc8 <16879988+fasc8@users.noreply.github.com>
2026-05-06 15:46:47 +0200

    set default view for diffview

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -584,7 +584,39 @@ require("lazy").setup({
         'notjedi/nvim-rooter.lua',
         config = function() require('nvim-rooter').setup() end
     }, { -- diff view for git diffs
-        'sindrets/diffview.nvim'
+        'sindrets/diffview.nvim',
+        opts = {
+            view = {
+                -- Configure the layout and behavior of different types of views.
+                -- Available layouts:
+                --  'diff1_plain'
+                --    |'diff2_horizontal'
+                --    |'diff2_vertical'
+                --    |'diff3_horizontal'
+                --    |'diff3_vertical'
+                --    |'diff3_mixed'
+                --    |'diff4_mixed'
+                -- For more info, see |diffview-config-view.x.layout|.
+                default = {
+                    -- Config for changed files, and staged files in diff views.
+                    layout = "diff2_horizontal",
+                    disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+                    winbar_info = false -- See |diffview-config-view.x.winbar_info|
+                },
+                merge_tool = {
+                    -- Config for conflicted files in diff views during a merge or rebase.
+                    layout = "diff3_mixed",
+                    disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+                    winbar_info = true -- See |diffview-config-view.x.winbar_info|
+                },
+                file_history = {
+                    -- Config for changed files in file history views.
+                    layout = "diff2_horizontal",
+                    disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+                    winbar_info = false -- See |diffview-config-view.x.winbar_info|
+                }
+            }
+        }
     }, -- Functional improvements
     { -- fzf support
         'ibhagwan/fzf-lua',
